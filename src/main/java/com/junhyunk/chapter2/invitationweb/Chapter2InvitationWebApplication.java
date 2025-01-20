572ac2c4fb03619a7a49307563911deb093f0494
Junhyun Kim <kjunh816@gmail.com>
2025-01-20 21:30:01 +0900

    init

---
+++ src/main/java/com/junhyunk/chapter2/invitationweb/Chapter2InvitationWebApplication.java
@@ -0,0 +1,13 @@
+package com.junhyunk.chapter2.invitationweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter2InvitationWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter2InvitationWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junhyunk/chapter2/invitationweb/Chapter2InvitationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junhyunk.chapter2.invitationweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter2InvitationWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
