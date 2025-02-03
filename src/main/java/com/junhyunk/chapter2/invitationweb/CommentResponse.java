ea2e4c11a9ffb7ff7462bece978608e7e7767128
Junhyun Kim <kjunh816@gmail.com>
2025-02-03 21:41:51 +0900

    feat: Remove backend module

--- src/main/java/com/junhyunk/chapter2/invitationweb/CommentResponse.java
+++
@@ -1,8 +0,0 @@
-package com.junhyunk.chapter2.invitationweb;
-
-public record CommentResponse(String name, String content) {
-
-    public static CommentResponse fromEntity(Comment comment) {
-        return new CommentResponse(comment.getName(), comment.getContent());
-    }
-}
--- src/main/java/com/junhyunk/chapter2/invitationweb/CreateCommentRequest.java
+++
@@ -1,4 +0,0 @@
-package com.junhyunk.chapter2.invitationweb;
-
-public record CreateCommentRequest(String name, String content) {
-}
